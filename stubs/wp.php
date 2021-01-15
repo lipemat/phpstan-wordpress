c348c04731675104bee6e499e4c558efe9add6d0
lipemat <mat@matlipe.com>
2021-01-15 14:22:11 -0700

    Add security hash constants to WP stubs

--- stubs/wp.php
+++ stubs/wp.php
@@ -2,7 +2,15 @@
 
 namespace {
 	const ABSPATH = '';
+	const AUTH_KEY = '';
+	const AUTH_SALT= '';
+	const LOGGED_IN_KEY= '';
+	const LOGGED_IN_SALT= '';
+	const NONCE_KEY = '';
+	const NONCE_SALT = '';
 	const SCRIPT_DEBUG = true;
+	const SECURE_AUTH_KEY = '';
+	const SECURE_AUTH_SALT = '';
 	const WP_CONTENT_DIR = '';
 	const WP_PLUGIN_DIR = '';
 }

bf4a3425cc58049045696c707c676a48052ca933
lipemat <mat@matlipe.com>
2021-07-08 14:41:03 -0400

    Declare more of the built in WP constants

--- stubs/wp.php
+++ stubs/wp.php
@@ -2,15 +2,23 @@
 
 namespace {
 	const ABSPATH = '';
+	const ADMIN_COOKIE_PATH = '';
+	const AUTH_COOKIE = '';
 	const AUTH_KEY = '';
-	const AUTH_SALT= '';
-	const LOGGED_IN_KEY= '';
-	const LOGGED_IN_SALT= '';
+	const AUTH_SALT = '';
+	const COOKIEPATH = '';
+	const COOKIE_DOMAIN = '';
+	const LOGGED_IN_COOKIE = '';
+	const LOGGED_IN_KEY = '';
+	const LOGGED_IN_SALT = '';
 	const NONCE_KEY = '';
 	const NONCE_SALT = '';
+	const PLUGINS_COOKIE_PATH = '';
 	const SCRIPT_DEBUG = true;
+	const SECURE_AUTH_COOKIE = '';
 	const SECURE_AUTH_KEY = '';
 	const SECURE_AUTH_SALT = '';
+	const SITECOOKIEPATH = '';
 	const WP_CONTENT_DIR = '';
 	const WP_PLUGIN_DIR = '';
 }

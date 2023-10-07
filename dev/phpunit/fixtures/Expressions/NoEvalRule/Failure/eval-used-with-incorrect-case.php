67ba536ea75945bcf4fc87bf1088871590922bab
lipemat <mat@matlipe.com>
2023-10-07 12:01:00 -0500

    Kill the `eval` and `@` rules
    These rules are already covered by PHPCS.

--- dev/phpunit/fixtures/Expressions/NoEvalRule/Failure/eval-used-with-incorrect-case.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Lipe\Lib\Phpstan\Rules\Test\Fixture\Expressions\NoEvalRule\Failure;
-
-eVaL('echo $foo');
--- dev/phpunit/tests/Rules/Expressions/NoCompactRuleTest.php
+++ dev/phpunit/tests/Rules/Expressions/NoCompactRuleTest.php
@@ -1,64 +1,61 @@
 <?php
 
-declare(strict_types=1);
+declare( strict_types=1 );
 
 namespace Lipe\Lib\Phpstan\Rules\Expressions;
 
 use Lipe\Lib\Phpstan\Rules\AbstractTestCase;
-use Lipe\Lib\Phpstan\Rules\Expressions;
 use PHPStan\Rules;
 
-final class NoCompactRuleTest extends AbstractTestCase
-{
-    public static function provideCasesWhereAnalysisShouldSucceed(): iterable
-    {
-        $paths = [
-            'compact-not-used' => __DIR__ . '/../../../fixtures/Expressions/NoCompactRule/Success/compact-not-used.php',
-        ];
+final class NoCompactRuleTest extends AbstractTestCase {
+	public static function provideCasesWhereAnalysisShouldSucceed(): iterable {
+		$paths = [
+			'compact-not-used' => __DIR__ . '/../../../fixtures/Expressions/NoCompactRule/Success/compact-not-used.php',
+		];
 
-        foreach ($paths as $description => $path) {
-            yield $description => [
-                $path,
-            ];
-        }
-    }
+		foreach ( $paths as $description => $path ) {
+			yield $description => [
+				$path,
+			];
+		}
+	}
 
-    public static function provideCasesWhereAnalysisShouldFail(): iterable
-    {
-        $paths = [
-            'compact-used-with-alias' => [
-                __DIR__ . '/../../../fixtures/Expressions/NoCompactRule/Failure/compact-used-with-alias.php',
-                [
-                    'Function compact() should not be used.',
-                    12,
-                ],
-            ],
-            'compact-used-with-correct-case' => [
-                __DIR__ . '/../../../fixtures/Expressions/NoCompactRule/Failure/compact-used-with-correct-case.php',
-                [
-                    'Function compact() should not be used.',
-                    10,
-                ],
-            ],
-            'compact-used-with-incorrect-case' => [
-                __DIR__ . '/../../../fixtures/Expressions/NoCompactRule/Failure/compact-used-with-incorrect-case.php',
-                [
-                    'Function compact() should not be used.',
-                    10,
-                ],
-            ],
-        ];
 
-        foreach ($paths as $description => [$path, $error]) {
-            yield $description => [
-                $path,
-                $error,
-            ];
-        }
-    }
+	public static function provideCasesWhereAnalysisShouldFail(): iterable {
+		$paths = [
+			'compact-used-with-alias'          => [
+				__DIR__ . '/../../../fixtures/Expressions/NoCompactRule/Failure/compact-used-with-alias.php',
+				[
+					'Function compact() should not be used.',
+					12,
+				],
+			],
+			'compact-used-with-correct-case'   => [
+				__DIR__ . '/../../../fixtures/Expressions/NoCompactRule/Failure/compact-used-with-correct-case.php',
+				[
+					'Function compact() should not be used.',
+					10,
+				],
+			],
+			'compact-used-with-incorrect-case' => [
+				__DIR__ . '/../../../fixtures/Expressions/NoCompactRule/Failure/compact-used-with-incorrect-case.php',
+				[
+					'Function compact() should not be used.',
+					10,
+				],
+			],
+		];
 
-    protected function getRule(): Rules\Rule
-    {
-        return new Expressions\NoCompactRule();
-    }
+		foreach ( $paths as $description => [$path, $error] ) {
+			yield $description => [
+				$path,
+				$error,
+			];
+		}
+	}
+
+
+	protected function getRule(): Rules\Rule {
+		return new NoCompactRule();
+	}
 }

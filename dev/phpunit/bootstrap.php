5bc5689cea0c12a7f3690aae42ca87ecefd002c8
lipemat <mat@matlipe.com>
2026-07-31 16:13:16 -0400

    Do not autoload the phpunit stubs for Lib\Container
    Overrides local project which are not yet on version 6 of wordpress-libs
     causing scanning errors

--- dev/phpunit/bootstrap.php
+++ dev/phpunit/bootstrap.php
@@ -6,5 +6,6 @@ ini_set( 'memory_limit', '512M' );
 
 require_once dirname( __DIR__, 2 ) . '/vendor/autoload.php';
 
+require __DIR__ . '/stubs/Container/Factory.php';
 require __DIR__ . '/helpers/StaticRule.php';
 require __DIR__ . '/tests/Rules/AbstractTestCase.php';

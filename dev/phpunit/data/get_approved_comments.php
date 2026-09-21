3c6569f9c97c9d7a7b48a198729fdd00412d2c11
lipemat <mat@matlipe.com>
2026-09-21 11:29:41 -0400

    Alias wordpress stubs to version 6.9.4
    Workaround until hp-stubs/wp-cli-stubs stubs releases new version which supports
     php-stubs/wordpress-stubs stubs 7.0

--- dev/phpunit/data/get_approved_comments.php
+++ dev/phpunit/data/get_approved_comments.php
@@ -6,20 +6,20 @@ use function PHPStan\Testing\assertType;
 
 assertType( 'array<int, WP_Comment>', get_approved_comments( 1 ) );
 
-assertType( 'int', get_approved_comments( 1, [
+assertType( 'int<0, max>', get_approved_comments( 1, [
 	'count' => true,
 ] ) );
-assertType( 'int', get_approved_comments( 1, [
+assertType( 'int<0, max>', get_approved_comments( 1, [
 	'count'  => true,
 	'fields' => 'ids',
 ] ) );
 assertType( 'array<int, WP_Comment>', get_approved_comments( 1, [
 	'count' => false,
 ] ) );
-assertType( 'array<int, int>', get_approved_comments( 1, [
+assertType( 'array<int<0, max>>', get_approved_comments( 1, [
 	'fields' => 'ids',
 ] ) );
-assertType( 'array<int, int>', get_approved_comments( 1, [
+assertType( 'array<int<0, max>>', get_approved_comments( 1, [
 	'count'  => false,
 	'fields' => 'ids',
 ] ) );
